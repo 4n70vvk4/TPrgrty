ddcc9cedb2c5b5f3176d188a8d0c6b98881b56f2
4n70vvk4 <113576903+4n70vvk4@users.noreply.github.com>
2023-03-04 17:16:36 +0300

    Add files via upload

---
+++ Lab_3-4/Lab3/Controllers/GeneralController.cs
@@ -0,0 +1,136 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Lab3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeneralController : ControllerBase
+    {
+        private readonly ConsultantContext _сontext;
+
+
+        public GeneralController(ConsultantContext сontext)
+        {
+            _сontext = сontext;
+        }
+        //GET
+        [HttpGet("GetFull")]
+        public async Task<ActionResult<IEnumerable<Consultant>>> GetConsultants()
+        {
+            if (_сontext.Consultants == null)
+            {
+                return NotFound();
+            }
+            return await _сontext.Consultants.ToListAsync();
+        }
+
+        //GET ID
+        [HttpGet("Consultant/{id}")]
+        public async Task<ActionResult<Consultant>> GetConsultant(int id)
+        {
+            if (_сontext.Consultants == null)
+            {
+                return NotFound();
+            }
+            var consultant = await _сontext.Consultants.FindAsync(id);
+            if (consultant == null)
+            {
+                return NotFound();
+            }
+
+            return consultant;
+        }
+        //GET W_ID
+        [HttpGet("Order/{id}")]
+        public async Task<ActionResult<Order>> GetOrder(int id)
+        {
+            if (_сontext.Orders == null)
+            {
+                return NotFound();
+            }
+            var order = await _сontext.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return order;
+        }
+        //POST 
+        [HttpPost("Consultant")]
+        public async Task<ActionResult<Consultant>> PostConsultant(Consultant consultant)
+        {
+            _сontext.Consultants.Add(consultant);
+            await _сontext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetConsultant), new { id = consultant.EmployeeID }, consultant);
+        }
+        [HttpPost("Order")]
+        public async Task<ActionResult<Order>> PostOrder(Order order)
+        {
+            _сontext.Orders.Add(order);
+            await _сontext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetConsultant), new { id = order.OrderID }, order);
+        }
+        [HttpPut("Consultant/{id}")]
+        public Task<IActionResult> PutConsultant(int id, Consultant consultant)
+        {
+            if (id != consultant.EmployeeID)
+            {
+                return Task.FromResult<IActionResult>(BadRequest());
+            }
+            return Task.FromResult<IActionResult>(NoContent());
+        }
+        [HttpPut("Order/{id}")]
+        public IActionResult PutOrder(int id, Order order)
+        {
+            if (id != order.OrderID)
+            {
+                return BadRequest();
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("Consultant/{id}")]
+        public async Task<IActionResult> DeleteConsultant(int id)
+        {
+            if (_сontext.Consultants == null)
+            {
+                return NotFound();
+            }
+            var consultant = await _сontext.Consultants.FindAsync(id);
+            if (consultant == null)
+            {
+                return NotFound();
+            }
+
+            _сontext.Consultants.Remove(consultant);
+            await _сontext.SaveChangesAsync();
+
+            return NoContent();
+        }
+        [HttpDelete("Order/{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            if (_сontext.Orders == null)
+            {
+                return NotFound();
+            }
+            var order = await _сontext.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _сontext.Orders.Remove(order);
+            await _сontext.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Lab_3-4/Lab3/Migrations/20221214172034_Company.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lab3.Migrations
+{
+    /// <inheritdoc />
+    public partial class Company : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Client = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Consultant = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date_and_time_of_delivery = table.Column<DateTime>(type: "datetime", nullable: true),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Amount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Consultants",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Post = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consultants", x => x.EmployeeID);
+                    table.ForeignKey(
+                        name: "FK_Consultants_Orders_OrderID",
+                        column: x => x.EmployeeID,
+                        principalTable: "Orders",
+                        principalColumn: "OrderID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consultants_OrderID",
+                table: "Consultants",
+                column: "OrderID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Consultants");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
