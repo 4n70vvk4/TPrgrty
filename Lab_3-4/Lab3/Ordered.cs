1751d93e75dd5b5ec6beefc2edffa11a40f50144
4n70vvk4 <113576903+4n70vvk4@users.noreply.github.com>
2023-03-04 17:15:12 +0300

    Add files via upload

---
+++ Lab_3-4/Lab3/Ordered.cs
@@ -0,0 +1,11 @@
+namespace Lab3
+{
+    public class Ordered
+    {
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public int Cost { get; set; }
+        public int Amount { get; set; }
+        public string? Discount { get; set; }
+    }
+}
